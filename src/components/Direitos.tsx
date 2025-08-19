44152e0aebbfafbc1537335d9006c00733628266
Rudrian <162451660+Rudrian@users.noreply.github.com>
2025-08-19 11:39:23 -0300

    Update Direitos.tsx

--- src/components/Direitos.tsx
+++ src/components/Direitos.tsx
@@ -1,6 +1,6 @@
 export default function Direitos() {
     return (
-        <footer className="bg-neutral-700 text-white text-center py-4 fixed bottom-0 w-full">
+        <footer className="bg-neutral-700 text-white text-center p-4 fixed bottom-0 w-full">
             <p>&copy; 2025 - SENAI. Todos os direitos reservados.</p>
         </footer>
     );
