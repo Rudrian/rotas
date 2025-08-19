0c9f066a7000b2860a11f46c003f2c58d349833d
Rudrian <162451660+Rudrian@users.noreply.github.com>
2025-08-19 11:31:59 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar/>
-        <Direitos/>
         {children}
+        <Direitos/>
       </body>
     </html>
   );
